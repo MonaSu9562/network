4761e3fb703701b6a649fbbdcdc590f265c6019d
MonaSu9562 <sumengyuan9562@gmail.com>
2017-11-01 11:41:40 +0000

    establish the project

---
+++ CS5001-p3-networking/src/WebServerMain.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author 170026060
+ *
+ */
+public class WebServerMain {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
